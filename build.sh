594ad7d8a7a556c3811c6f408e9c4437727a34db
sqweek <sqweek@gmail.com>
2017-07-18 22:11:30 +0800

    Add MIT/Lucent Public licenses.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+if ! [ -e ./keyboard ]; then
+	echo >&2 "./keyboard does not exist; use builtin lib/keyboard?"
+	echo >&2 "(governed by Lucent Public License, see lib/LICENSE.plan9)"
+	read >&2 -p "[y/n] " -n 1
+	echo
+	case "$REPLY" in
+		y*|Y*) cp lib/keyboard keyboard;;
+		*) echo >&2 aborting; exit  1;;
+	esac
+fi
 CC=gcc
+set -x
 ${CC} genkbd.c -o genkbd  &&
 ./genkbd <keyboard >kbd.h  &&
 ${CC} -mwindows hook.c -o 9ime
